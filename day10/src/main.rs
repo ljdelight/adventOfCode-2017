39de907955ffcc48120fb9834343a70cc6fe7fe9
ljdelight <ljdelight@gmail.com>
2017-12-11 22:28:54 -0600

    Solution for day 10 part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -19,23 +19,64 @@ fn reverse_span(seq: &mut Vec<i32>, start_idx: i32, length: i32) {
     }
 }
 
+fn do_round(seq: &mut Vec<i32>, length_list: &Vec<i32>, offset: &mut i32, skip_size: &mut i32) {
+    for length in length_list {
+        reverse_span(seq, *offset, *length);
+        *offset += ((*length + *skip_size) % seq.len() as i32);
+        *skip_size += 1;
+    }
+}
+
+fn str_expand_to_binary(seq: &str) -> Vec<i32> {
+    seq.bytes().map(|it| it as u8).map(|it| it as i32).collect()
+}
+
 fn main() {
     tests();
     let input = "187,254,0,81,169,219,1,190,19,102,255,56,46,32,2,216";
-    let length_list = input.split(',').map(|it| it.parse().unwrap());
+    println!("Solution for part 1: {}", part1(&input));
+    println!("Solution for part 2: {}", knot_hash(&input));
+}
+
+fn part1(input: &str) -> i32 {
+    let length_list: Vec<i32> = input.split(',').map(|it| it.parse().unwrap()).collect();
     let mut seq = vec_seq(256);
     let mut offset = 0i32;
-    let mut skipSize = 0;
-    println!("Original {:?}", seq);
-    for length in length_list {
-        reverse_span(&mut seq, offset, length);
-        offset += ((length + skipSize) % seq.len() as i32);
-        skipSize += 1;
+    let mut skip_size = 0;
+    // println!("Original {:?}", seq);
+    do_round(&mut seq, &length_list, &mut offset, &mut skip_size);
+    seq[0] * seq[1]
+}
+
+fn dense_hash(array: &Vec<i32>) -> u8 {
+    let mut xor = 0;
+    for val in array {
+        xor ^= *val as u8;
     }
-    println!("Reverse {:?}", seq);
-    println!("Part 1: {}*{} = {}", seq[0], seq[1], seq[0] * seq[1]);
+    xor
+}
 
+fn knot_hash(input: &str) -> String {
+    let mut input = str_expand_to_binary(input);
+    input.extend(vec![17, 31, 73, 47, 23]);
 
+    let input = input;
+    let mut seq = vec_seq(256);
+    let mut offset = 0i32;
+    let mut skip_size = 0;
+    // println!("Original {:?}", seq);
+    for round in 0..64 {
+        do_round(&mut seq, &input, &mut offset, &mut skip_size);
+    }
+    let len = seq.len() / 16;
+    let mut res = String::from("");
+    for subset in 0..len {
+        let slice = &seq[0 + 16 * subset..16 * (subset + 1)];
+        let s = dense_hash(&slice.to_vec());
+        let c = format!("{:02x}", s);
+        res.push_str(&c);
+    }
+    res
 }
 
 fn tests() {
@@ -80,4 +121,12 @@ fn tests() {
     }
     assert!(seq[0] * seq[1] == 12);
 
+    // Part 2 tests
+    assert!(str_expand_to_binary("1,2,3") == vec![49, 44, 50, 44, 51]);
+    assert!(dense_hash(&vec![65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22]) == 64u8);
+
+    assert!(knot_hash("") == "a2582a3a0e66e6e86e3812dcb672a272");
+    assert!(knot_hash("AoC 2017") == "33efeb34ea91902bb2f59c9920caa6cd");
+    assert!(knot_hash("1,2,3") == "3efbe78a8d82f29979031a4aa0b16a9d");
+    assert!(knot_hash("1,2,4") == "63960835bcdc130f0b66d7ff4f6a5a8e");
 }
